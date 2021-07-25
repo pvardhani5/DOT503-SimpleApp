584b634cc774b0cb49fc98612ed2a51f45effc27
pvardhani5 <87924006+pvardhani5@users.noreply.github.com>
2021-07-25 19:10:18 +1000

    Add files via upload

---
+++ src/default.ts
@@ -0,0 +1,8 @@
+//Default
+export function getHeader() {
+    return `<a href="/">Home</a> | <a href="/even">Even</a> | <a href="/odd">Odd</a> | <a href="/prime">Prime</a> | <a href="/composite">Composite</a><br>`
+}
+
+export function getFooter() {
+    return `<br>`
+}
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+//Index
+import { App } from "./app";
+
+App.start()
+App.config({
+    first: 1,
+    last: 100
+})
