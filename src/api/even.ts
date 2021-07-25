084870ee0f86e01157b2f9f538e8b282a786a2e2
pvardhani5 <87924006+pvardhani5@users.noreply.github.com>
2021-07-25 19:09:33 +1000

    Add files via upload

---
+++ src/api/even.ts
@@ -0,0 +1,15 @@
+//Events
+import { Numbers } from "./classes/Numbers";
+
+export function getEven(first: number, last: number) {
+
+    const numbers = new Numbers(first, last)
+    const even = numbers.even()
+
+    let response = ``
+    even.forEach((n) => {
+        response += ` ${n} `
+    })
+
+    return `${response}`
+}
